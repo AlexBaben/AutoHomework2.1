25d26a53fca4fd59bd83695dd67b8b5e43a47183
Alex Babenko <wertuniul@gmail.com>
2023-06-03 13:27:48 +0300

    AutoHomework2.1/fix

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -68,10 +68,9 @@ class CallbackTest {
         form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79270000000");
         form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         form.findElement(By.cssSelector("button")).click();
-        String text = driver.findElement(By.cssSelector(".input_invalid .input__sub")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id=name] .input_invalid .input__sub")).getText();
         assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
     }
-
     @Test
     void shouldNotTestNoName() {
         WebElement form = driver.findElement(By.cssSelector("form"));
@@ -79,7 +78,7 @@ class CallbackTest {
         form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79270000000");
         form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         form.findElement(By.cssSelector("button")).click();
-        String text = driver.findElement(By.cssSelector(".input_invalid .input__sub")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id=name] .input_invalid .input__sub")).getText();
         assertEquals("Поле обязательно для заполнения", text.trim());
     }
 
@@ -90,7 +89,7 @@ class CallbackTest {
         form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+792700000000");
         form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         form.findElement(By.cssSelector("button")).click();
-        String text = driver.findElement(By.cssSelector(".input_invalid .input__sub")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id=phone] .input_invalid .input__sub")).getText();
         assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
     }
 
@@ -101,7 +100,7 @@ class CallbackTest {
         form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("");
         form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         form.findElement(By.cssSelector("button")).click();
-        String text = driver.findElement(By.cssSelector(".input_invalid .input__sub")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id=phone] .input_invalid .input__sub")).getText();
         assertEquals("Поле обязательно для заполнения", text.trim());
     }
 
